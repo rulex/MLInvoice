cd5223d291e6b4f71f0dd33a1262b47af1bf80f0
Lars Wikberg <lars.wikberg@gmail.com>
2015-12-02 17:31:56 +0200

    merge with upstream

--- version.php
+++ version.php
@@ -1,18 +1,17 @@
 <?php
 /*******************************************************************************
-MLInvoice: web-based invoicing application.
-Copyright (C) 2010-2015 Ere Maijala
+ MLInvoice: web-based invoicing application.
+ Copyright (C) 2010-2015 Ere Maijala
 
-This program is free software. See attached LICENSE.
+ This program is free software. See attached LICENSE.
 
-*******************************************************************************/
+ *******************************************************************************/
 
 /*******************************************************************************
-MLInvoice: web-pohjainen laskutusohjelma.
-Copyright (C) 2010-2015 Ere Maijala
+ MLInvoice: web-pohjainen laskutusohjelma.
+ Copyright (C) 2010-2015 Ere Maijala
 
-Tämä ohjelma on vapaa. Lue oheinen LICENSE.
+ Tämä ohjelma on vapaa. Lue oheinen LICENSE.
 
-*******************************************************************************/
-
-$softwareVersion = '1.11.1';
+ *******************************************************************************/
+$softwareVersion = '1.12.0';
